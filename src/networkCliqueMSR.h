6d312eef002d62cfa33e53fe1cb7beab92ac2300
dadazhetang <139195634+dadazhetang@users.noreply.github.com>
2024-12-23 15:25:15 +0800

    Update networkCliqueMSR.h

--- src/networkCliqueMSR.h
+++ src/networkCliqueMSR.h
@@ -122,7 +122,7 @@ public:
 };
 
 // Function to initialize network
-Network createNetwork(Rcpp::DataFrame netdf, double exp = 3.5)
+Network createNetwork(Rcpp::DataFrame netdf, double exp = 4)
 {
   Network net;
   net.edges = createEdges(netdf);
